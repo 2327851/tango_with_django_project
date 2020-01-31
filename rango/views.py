0e4d8e8bbb238444878af00f291304390d8214b5
anna <2327851s@student.gla.ac.uk>
2020-01-31 15:17:20 +0200

    adding hyper link to about that leads to index page

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner  <a href=/rango/about/>Link to the about page</a>")
+    return HttpResponse("Rango says hey there partner  <a href=/rango/about/>Go to About</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page ")
+    return HttpResponse("Rango says here is the about page <a href=/index/>Go to Index</a>")
