453f038190461f94b04410b98666e1bfcfb5178a
anna <2327851s@student.gla.ac.uk>
2020-02-12 16:53:24 +0200

    added MEDIA_DIR

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,10 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,6 +116,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Media folder
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 
 # Static files (CSS, JavaScript, Images)
