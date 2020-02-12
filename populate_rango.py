970d4c1f08840b937e2fb6d2ba2d8aab2aaf6f35
anna <2327851s@student.gla.ac.uk>
2020-02-13 01:33:50 +0200

    fixing python pages titles not showing up

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,6 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'tango_with_django_project.settings')
 
 import django
 django.setup()
@@ -8,25 +9,25 @@ from rango.models import Category, Page
 def populate(): 
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+        'url':'http://docs.python.org/3/tutorial/'},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+        'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        'url':'http://www.korokithakis.net/tutorials/python/'} ]
          
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+        'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+        'url':'http://www.tangowithdjango.com/'} ]
          
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+        'url':'http://flask.pocoo.org'} ]
          
     cats = {'Python': {'pages': python_pages},
             'Django': {'pages': django_pages},
