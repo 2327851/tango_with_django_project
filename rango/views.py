7a7cee07722d2c2ce1ad223dcb6dab7602079e00
anna <2327851s@student.gla.ac.uk>
2020-02-14 08:50:02 +0200

    added ability to logout

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login 
+from django.contrib.auth import authenticate, login, logout 
 from django.contrib.auth.decorators import login_required
 from rango.models import Page
 from django.http import HttpResponse
@@ -135,3 +135,8 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
+
+@login_required
+def user_logout(request):
+    logout(request) 
+    return redirect(reverse('rango:index'))
